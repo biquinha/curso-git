1055b6d423e88c933c3294bd42353c79290c41d9
biquinha <ricardo.biquinha@gmail.com>
2019-12-19 00:16:35 +0000

    Segunda versão

--- projecto-teste/Program.cs
+++ projecto-teste/Program.cs
@@ -4,6 +4,7 @@ namespace projecto_teste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Alteração GIT versão 2");
             Console.ReadLine();
         }
     }
