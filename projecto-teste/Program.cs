aa848e3a0ee7f2455b85e22e4aebcaf566f930b7
biquinha <ricardo.biquinha@gmail.com>
2019-12-19 22:14:04 +0000

    vs.3 versão

--- projecto-teste/Program.cs
+++ projecto-teste/Program.cs
@@ -4,8 +4,8 @@ namespace projecto_teste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Alteração GIT versão 2");
-            Console.ReadLine();
+            Console.WriteLine("Alteração GIT versão 3");
+            Console.WriteLine("Tchau!");
         }
     }
 }
